949835715216ef652f4b7c301a22320ad0e0b213
Mikhail Kindulov <kindulov_ml@astrail.net>
2021-11-22 16:05:26 +0300

    [new] Basic setup.py.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="StrangeBoostings",
+    version="0.1",
+    packages=["strangeboostings",],
+    license=open("LICENSE").read(),
+    long_description="Nothing here...",
+)
